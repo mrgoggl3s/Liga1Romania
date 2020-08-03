95418da2813da94f9ef4b2a33f4d355c7f124387
catalincimpoeru <catalincimpoeru24@gmail.com>
2020-08-03 22:59:07 +0300

    add pla

--- src/Application.java
+++ src/Application.java
@@ -5,12 +5,22 @@ public class Application {
         Team craiova = new Team("U.Craiova");
         Team astra = new Team("Astra");
         Team botosani = new Team("Botosani");
+        Team botosani1 = new Team("Dinamo");
+        Team botosani2 = new Team("Rapid");
+        Team botosani3 = new Team("Mioveni");
+        Team botosani4 = new Team("Arges");
+        Team botosani5 = new Team("Slatina");
 
 
         TeamStorage.TEAM_STORAGE.add(cfr);
         TeamStorage.TEAM_STORAGE.add(craiova);
         TeamStorage.TEAM_STORAGE.add(astra);
         TeamStorage.TEAM_STORAGE.add(botosani);
+        TeamStorage.TEAM_STORAGE.add(botosani1);
+        TeamStorage.TEAM_STORAGE.add(botosani2);
+        TeamStorage.TEAM_STORAGE.add(botosani3);
+        TeamStorage.TEAM_STORAGE.add(botosani4);
+        TeamStorage.TEAM_STORAGE.add(botosani5);
 
         Championship championship = new Championship();
         championship.startChampionship();
